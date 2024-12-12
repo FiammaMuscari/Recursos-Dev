a0eebaca195636ceb540e24297e359979272396f
Fiammamuscari <fiammamuscari@gmail.com>
2024-12-12 00:01:17 -0300

    🎁 feat: generate dynamic sitemap

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -6,38 +6,38 @@ type TableRowProps = {
 };
 
 const predefinedColors = [
-  "#4CAF50",
-  "#2196F3",
-  "#9C27B0",
-  "#E91E63",
-  "#FFEB3B",
-  "#FF9800",
-  "#00BCD4",
-  "#673AB7",
-  "#FFC107",
-  "#FF5722",
+  "#39FF14", // Neon Green
+  "#FF6EC7", // Neon Pink
+  "#00FFFF", // Neon Cyan
+  "#FFFF33", // Neon Yellow
+  "#FFB300", // Neon Bright Orange
+  "#FF1493", // Neon Deep Pink
+  "#7FFF00", // Neon Chartreuse
+  "#32CD32", // Neon Lime Green
+  "#00FA9A", // Neon Medium Spring Green
+  "#FFD700", // Neon Gold
+  "#FF69B4", // Neon Hot Pink
+  "#BA55D3", // Neon Medium Orchid
+  "#00BFFF", // Neon Bright Blue
+  "#FF00FF", // Neon Magenta
+  "#FF6347", // Neon Tomato Red
 ];
 
 const tagColors: Record<string, { background: string; border: string }> = {};
 
-const getBackgroundColor = (color: string) => {
-  return `${color}33`;
-};
-
 const TableRow: React.FC<TableRowProps> = ({ entry }) => {
   return (
     <tr className="z-20 whitespace-nowrap">
-      <td className="px-4 py-2 ">{entry.name}</td>
-      <td className="px-4 py-2 ">
+      <td className="px-4 py-2">{entry.name}</td>
+      <td className="px-4 py-2">
         {entry.tags.map((tag) => {
           if (!tagColors[tag]) {
             const colorIndex =
               Object.keys(tagColors).length % predefinedColors.length;
             const borderColor = predefinedColors[colorIndex];
-            const backgroundColor = getBackgroundColor(borderColor);
 
             tagColors[tag] = {
-              background: backgroundColor,
+              background: "rgba(50, 50, 50, .50)",
               border: borderColor,
             };
           }
@@ -47,7 +47,7 @@ const TableRow: React.FC<TableRowProps> = ({ entry }) => {
           return (
             <span
               key={tag}
-              className="inline-block  mr-2 px-2 py-1 rounded"
+              className="inline-block mr-2 px-3 py-1 rounded-lg shadow hover:shadow-md transition-shadow duration-200 ease-in-out"
               style={{
                 backgroundColor: background,
                 borderColor: border,
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import path from "path";
+import fs from "fs";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { generateSitemap } from "sitemap-ts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "generate-sitemap",
+      closeBundle() {
+        const pagesDir = path.resolve(__dirname, "src");
+        const pageFiles = fs.readdirSync(pagesDir);
+        const dynamicRoutes = pageFiles
+          .filter((file) => path.extname(file) === ".tsx") 
+          .map((file) => {
+            const route = "/" + path.basename(file, ".tsx"); 
+            return route === "/" ? "/" : route; 
+          });
+
+
+        generateSitemap({
+          hostname: "https://recursos-dev-gamma.vercel.app/",
+          outDir: path.resolve(__dirname, "static"),
+          readable: true,
+          dynamicRoutes,
+        });
+      },
+    },
+  ],
+  build: {
+    outDir: "./static",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
